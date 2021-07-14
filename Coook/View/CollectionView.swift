ac2ab74886417c9c2fcc6d27877229da8e044fe8
Tim Searle <tim.searle@marks-and-spencer.com>
2021-07-14 12:17:27 +0100

    Add section headers

--- Coook/View/CollectionView.swift
+++ Coook/View/CollectionView.swift
@@ -19,11 +19,23 @@ struct CollectionView: View {
   var body: some View {
     ScrollView {
       ForEach(sectionedRecipes) { section in
-        LazyVGrid(columns: [GridItem(alignment: .top), GridItem(alignment: .top)]) {
-          ForEach(section.recipes) { recipe in
-            let viewModel = RecipeViewModel(recipe: recipe)
-            RecipeGridItem(viewModel: viewModel)
-              .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+        VStack {
+          HStack {
+            Image(systemName: "clock")
+            Text("\(section.mins) minutes total")
+              .bold()
+              .padding()
+            Spacer()
+          }
+          .background(.white)
+          .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+          .padding([.top, .bottom])
+          LazyVGrid(columns: [GridItem(alignment: .top), GridItem(alignment: .top)]) {
+            ForEach(section.recipes) { recipe in
+              let viewModel = RecipeViewModel(recipe: recipe)
+              RecipeGridItem(viewModel: viewModel)
+                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+            }
           }
         }
       }
@@ -36,7 +48,7 @@ struct CollectionView: View {
 
   var sectionedRecipes: [RecipeSection] {
     let sorted = allRecipes.sorted { left, right in
-      left.totalCookingTime < right.totalCookingTime
+      left.totalCookingTime > right.totalCookingTime
     }
 
     let sections = sorted.chunked(on: { $0.totalCookingTime }).map { RecipeSection(mins: $0.0, recipes: Array($0.1)) }
