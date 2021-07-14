5efbce9727e9587ce2cf2703017900e32841390a
Tim Searle <tim.searle@marks-and-spencer.com>
2021-07-14 11:26:12 +0100

    Added recipe searching

--- Coook/View/CollectionView.swift
+++ Coook/View/CollectionView.swift
@@ -3,10 +3,12 @@ import SwiftUI
 struct CollectionView: View {
   var viewModel: RecipesViewModel
 
+  @State private var searchText: String = ""
+
   var body: some View {
     ScrollView {
       LazyVGrid(columns: [GridItem(alignment: .top), GridItem(alignment: .top)]) {
-        ForEach(viewModel.recipes) { recipe in
+        ForEach(allRecipes) { recipe in
           let viewModel = RecipeViewModel(recipe: recipe)
           RecipeGridItem(viewModel: viewModel)
             .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
@@ -16,5 +18,14 @@ struct CollectionView: View {
     }
     .background(Color(.systemGroupedBackground))
     .navigationBarTitle(Text(viewModel.title))
+    .searchable(text: $searchText)
+  }
+
+  var allRecipes: [Recipe] {
+    guard searchText.count > 2 else {
+      return viewModel.recipes
+    }
+
+    return viewModel.recipes.filter { $0.containsKeyword(searchText) }
   }
 }
