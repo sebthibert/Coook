e9dee677c968efe8b5f2f1c37dd3407f42a6f3f8
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2021-07-15 14:37:16 +0100

    Remove space

--- Coook/View/ViewExtension.swift
+++ Coook/View/ViewExtension.swift
@@ -4,7 +4,7 @@ extension View {
   func shareSheet(showShareSheet: Binding<Bool>, items: [Any]) -> some View {
     self.modifier(ShareSheetModifer(showShareSheet: showShareSheet, shareSheetItems: items))
   }
-  
+
   @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
     if condition {
       transform(self)
